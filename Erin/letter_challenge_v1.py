a16feb18fc4c4c7c738550eae13bc363139d7028
Kevin <kevin.yw.cho@gmail.com>
2024-04-03 22:04:45 +1100

    letter challenge v1

--- Erin/letter_challenge_v1.py
+++ Erin/letter_challenge_v1.py
@@ -10,40 +10,24 @@ Display how many times a letter occurs twice in a row
 Must be able to work with both low and upper case letters (Case sensitive)
 """
 
-# Function to get two inputs - String and letter
-# 1. Check how many times a letter occurs in the string
-# 2. Check how many times a letter occurs twice in a row
-# Return output 1 and 2
-def letter_occurrence(string, letter):
+# Input string and letter
+target_string = input("Please input a string: ")
+target_letter = input("Please input a letter to count: ")
 
-    pre_element = ''
-    twice_in_a_row_count = 0
-    count = 0
+# Define variables
+pre_char = ''
+twice_in_a_row_count = 0
+count = 0
 
-    # For loop to check if a letter matches in that string
-    for e in string:
-        if e == letter: # If matched
-            count += 1 # Count how many times the letter occurs in that string
-            if e == pre_element: # If matched with previous letter
-                twice_in_a_row_count += 1 # Count how many times the letter occurs twice in a row
-        pre_element = e # Assign the letter into pre_element
+# For loop to check if a letter matches in that string
+for char in target_string:
+    if char == target_letter: # If matched
+        count += 1 # Count how many times the letter occurs in that string
+        if char == pre_char: # If matched with previous letter
+            twice_in_a_row_count += 1
+        pre_char = char # Assign the letter into pre_element
 
-    # Return two output
-    return count, twice_in_a_row_count
+# Print outcomes
+print(f"The target letter '{target_letter}' occurs {count} times in the string.")
+print(f"The target letter '{target_letter}' occurs twice in a row {twice_in_a_row_count} times in the string.")
 
-# Main function to print output
-def main():
-
-    # Input string
-    target_string = input("Please input a string: ")
-    target_letter = input("Please input a letter to count: ")
-
-    count_occurrence, count_twice_in_a_row = letter_occurrence(target_string, target_letter)
-
-    # Print outcomes
-    print(f"The target letter '{target_letter}' occurs {count_occurrence} times in the string.")
-    print(f"The target letter '{target_letter}' occurs twice in a row {count_twice_in_a_row} times in the string.")
-
-# Execute main function
-if __name__ == "__main__":
-    main()
